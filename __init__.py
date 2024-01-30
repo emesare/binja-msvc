b3335936444c996ab6e9a10d7cc6b50095326d63
Mason Reed <msr@emesare.com>
2024-01-29 19:10:30 -0500

    Download latest native build from github releases

--- __init__.py
+++ __init__.py
@@ -2,17 +2,14 @@ from pathlib import Path
 from platform import system
 from shutil import copy2
 from hashlib import md5
+from urllib.request import urlretrieve
 
 import binaryninja as bn
 import os
+import json
 
+LIB_PATH = Path(bn.user_plugin_path())
 PLUGIN_PATH = Path(bn.user_plugin_path()) / "msvc-native"
-
-mgr = bn.RepositoryManager()
-for repo in mgr.repositories:
-	if any([x.path == "emesare_msvc_native" and x.installed for x in repo.plugins]):
-		PLUGIN_PATH = Path(repo.full_path) / "emesare_msvc_native"
-		break
 
 LIB_NAME = "libbinja-msvc.so"
 if system() == "Windows":
@@ -20,19 +17,35 @@ if system() == "Windows":
 elif system() == "Darwin":
 	LIB_NAME = "libbinja-msvc.dylib"
 
-LIB_PATH = Path(bn.user_plugin_path()) / LIB_NAME
-LIB_SRC = PLUGIN_PATH / LIB_NAME
+DOWNLOAD_URL = "https://github.com/emesare/binja-msvc/releases/latest/download/" + LIB_NAME
+
+# Swap PLUGIN_PATH to use repository path if plugin exists in there.
+mgr = bn.RepositoryManager()
+for repo in mgr.repositories:
+	if any([x.path == "emesare_msvc_native" and x.installed for x in repo.plugins]):
+		PLUGIN_PATH = Path(repo.full_path) / "emesare_msvc_native"
+		break
+
+def needs_install():
+    # If First run, install
+    if not (LIB_PATH / LIB_NAME).exists(follow_symlinks=True):
+        return True
+    # If our plugin updates the manifest version will differ.
+    with open(PLUGIN_PATH / "plugin.json") as manifest_file:
+        manifest = json.load(manifest_file)
+        with open(LIB_PATH / "emesare_msvc_native.json") as installed_manifest_file:
+            installed_manifest = json.load(installed_manifest_file)
+            if manifest["version"] != installed_manifest["version"]:
+                return True
+    return False
+        
 
 def update_binary():
-	copy2(LIB_SRC, LIB_PATH)
-	bn.log_warn("libbinja-msvc installed, please restart")
+    urllib.urlretrieve(DOWNLOAD_URL, LIB_INSTALL_PATH / LIB_NAME)
+    # Copy over the manifest to compare against in the future.
+    copy2(LIB_MANIFEST, LIB_INSTALL_PATH / "emesare_msvc_native.json")
+    bn.log_warn("binja-msvc installed, please restart!")
 
-if not LIB_PATH.exists(follow_symlinks=True):
-	update_binary()
-else:
-	OLD_CHECKSUM = md5(open(LIB_SRC,"rb").read()).hexdigest()
-	NEW_CHECKSUM = md5(open(LIB_PATH,"rb").read()).hexdigest()
-
-	if OLD_CHECKSUM != NEW_CHECKSUM:
-		bn.log_info("Old libbinja-msvc found, updating...")
-		update_binary()
+if needs_install():
+    bn.log_info("installing binja-msvc...")
+    update_binary()
